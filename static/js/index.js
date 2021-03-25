3512217b7fe779d40bfd24538f9d358ca2fc158b
Grace2614 <80277169+Grace2614@users.noreply.github.com>
2021-03-25 17:10:07 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 //https://www.eclipse.org/paho/clients/js/
-
+/*
 function LED1_On() {
 	//alert("led on");
 	console.log("led on");
@@ -17,6 +17,25 @@ function LED1_Off(){
 	client.send(message);
 }
 
+var btn=document.getElementById('btn'),contador=0;
+function cambio()
+{if (contador==0)
+	{
+	message = new Paho.MQTT.Message("ENCENDER");
+ 	message.destinationName = "grace.bonilla@unach.edu.ec/tema1";
+ 	client.send(message);
+ 	contador=1;
+ 	}
+ else
+ 	{
+ 	message = new Paho.MQTT.Message("APAGAR");
+	message.destinationName = "grace.bonilla@unach.edu.ec/tema1";
+	client.send(message);
+ 	contador=0;
+ 	}
+	client.send(message);
+	}
+}
 
 
 
@@ -69,6 +88,12 @@ function LED1_Off(){
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
 	  document.getElementById("sensor").innerHTML=message.payloadString;
-	  
-	  
-  }
+          if(message.payloadString==='Encendido'){
+                   document.getElementById("imagen").src="https://www.jing.fm/clipimg/full/114-1141391_homer-simpson-fighting-clipart-homer-simpson-bart-simpson.png"}
+	else if (message.payloadString'Apagado'){
+                document.getElementById("imagen").src="https://i.pinimg.com/736x/db/e0/3b/dbe03b8735e70b6461278393c5769258.jpg "}
+	  if(message.payloadString==='Encendido'){
+                  document.getElementById("btn").innerHTML="Apagar";}
+          else if (message.payloadString'Apagado'){
+                  document.getElementById("btn").innerHTML="Encender";}
+	    }
