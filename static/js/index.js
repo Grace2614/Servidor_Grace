b8c7f97e5517478bab0eaf76adc4e37156af32e5
Grace2614 <80277169+Grace2614@users.noreply.github.com>
2021-03-25 17:12:14 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,4 @@
 //https://www.eclipse.org/paho/clients/js/
-/*
-function LED1_On() {
-	//alert("led on");
-	console.log("led on");
-	//document.getElementById("sensor").innerHTML="led on";
-	message = new Paho.MQTT.Message("ON");
-	message.destinationName = "grace.bonilla@unach.edu.ec/tema1";
-	client.send(message);
-}
-function LED1_Off(){	
-	//alert("led off");
-	console.log("led off");
-	//document.getElementById("sensor").innerHTML="led off";
-	message = new Paho.MQTT.Message("OFF");
-	message.destinationName = "grace.bonilla@unach.edu.ec/tema1";
-	client.send(message);
-}
 
 var btn=document.getElementById('btn'),contador=0;
 function cambio()
